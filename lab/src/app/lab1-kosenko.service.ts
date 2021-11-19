f787c081f25b8b1a2f0659cf90304a94baec45a2
Peter <kosenko.petro1@gmail.com>
2021-11-19 19:20:29 +0200

    Lab2 initial commit

--- lab/src/app/lab1-kosenko.service.ts
+++ lab/src/app/lab1-kosenko.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Lab1KosenkoService {
+  getNumbers(): number {
+    return -1 * 5;
+  }
+  getHello(): string {
+    return "Hello";
+  }
+  getArrays(): number[] {
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  }
+  getEnums(): Role {
+    return Role.Admin;
+  }
+  getMultiTypeArray(): (string|number)[] {
+    return [1, "Hello"];
+  }
 
-  constructor() { }
+  getMultiTypeValue(): string|number {
+    return 5;
+  }
+}
+
+enum Role{
+  Admin,
+  User
 }
