35bd982f020bf76c439cc6422913a3a251ec8bf6
Peter <kosenko.petro1@gmail.com>
2021-11-20 12:30:18 +0200

    Lab3 initial commit

--- lab/src/app/lab1-kosenko.service.ts
+++ lab/src/app/lab1-kosenko.service.ts
@@ -7,9 +7,6 @@ export class Lab1KosenkoService {
   getNumbers(): number {
     return -1 * 5;
   }
-  getHello(): string {
-    return "Hello";
-  }
   getArrays(): number[] {
     return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   }
@@ -22,6 +19,17 @@ export class Lab1KosenkoService {
 
   getMultiTypeValue(): string|number {
     return 5;
+  }
+
+  // Lab3
+  private static getBiggerNumber(a: number, b: number): number {
+    return (a > b) ? a : b;
+  }
+
+  getHello(): string {
+    const a = 45;
+    const b = -75;
+    return "Lab 3 result: " + Lab1KosenkoService.getBiggerNumber(a, b).toString();
   }
 }
 
